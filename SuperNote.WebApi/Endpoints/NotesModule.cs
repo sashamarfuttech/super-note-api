f9e0aa2d7e5a19e42148ab1e08be15b859ea6930
oleksandr.marfut <oleksandr.marfut@taxsystems.com>
2024-02-01 13:48:55 +0100

    test endpoint added

---
+++ SuperNote.WebApi/Endpoints/NotesModule.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperNote.WebApi.Endpoints;
+
+public static class NotesModule
+{
+    public static void AddNotesEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/notes", () =>
+        {
+            return Results.Ok("my notes");
+        });
+    }
+}
--- SuperNote.WebApi/Program.cs
+++ SuperNote.WebApi/Program.cs
@@ -1,16 +1,9 @@
-using SuperNote.Application;
-using SuperNote.Infrastructure;
+using SuperNote.WebApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services
-    .AddDomainServices()
-    .AddApplicationServices()
-    .AddInfrastructureServices()
-    .AddPresentationServices();
 
 var app = builder.Build();
 
@@ -19,5 +12,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.AddNotesEndpoints();
 
 app.Run();
